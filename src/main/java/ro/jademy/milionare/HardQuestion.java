c718716d0db6779e9fb17a2cbaad8869cff8ea79
Zarnescu Cosmin <cosminzarnescu@gmail.com>
2020-07-17 16:57:35 +0300

    Second Commit

--- src/main/java/ro/jademy/milionare/HardQuestion.java
+++
@@ -1,9 +0,0 @@
-package ro.jademy.milionare;
-
-import java.util.ArrayList;
-
-public class HardQuestion extends Question {
-    public HardQuestion(String question, ArrayList<Answer> answers, int value) {
-        super(question, answers, value);
-    }
-}
